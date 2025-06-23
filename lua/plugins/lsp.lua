4ea39dea992cd53fba607b53a412fe3de1d3c825
Thomas Steck <thomas@steckoverflow.se>
2025-06-23 13:16:23 +0200

    python lsp fixes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,7 +92,34 @@ return { -- LSP Configuration & Plugins
 					},
 				},
 				ruff = {},
-				basedpyright = {},
+				basedpyright = {
+					before_init = function(_, c)
+						if not c.settings then
+							c.settings = {}
+						end
+						if not c.settings.python then
+							c.settings.python = {}
+						end
+						c.settings.python.pythonPath = vim.fn.exepath("python")
+					end,
+					settings = {
+						basedpyright = {
+							analysis = {
+								typeCheckingMode = "basic",
+								autoImportCompletions = true,
+								diagnosticSeverityOverrides = {
+									reportUnusedImport = "information",
+									reportUnusedFunction = "information",
+									reportUnusedVariable = "information",
+									reportGeneralTypeIssues = "none",
+									reportOptionalMemberAccess = "none",
+									reportOptionalSubscript = "none",
+									reportPrivateImportUsage = "none",
+								},
+							},
+						},
+					},
+				},
 				debugpy = {},
 				gopls = {},
 				delve = {},
