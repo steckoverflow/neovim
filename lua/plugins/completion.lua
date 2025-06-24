5c15767ca9933c6a9a9e38a36809276730b1cd40
Thomas Steck <thomas@steckoverflow.se>
2025-06-24 09:55:59 +0200

    trying the go pkg completion plugin

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -18,19 +18,18 @@ return {
 			"fang2hou/blink-copilot",
 			dependencies = "zbirenbaum/copilot.lua",
 		},
-		-- FIXME: Causes error
-		-- {
-		-- 	"Yu-Leo/cmp-go-pkgs",
-		-- 	enabled = vim.fn.executable("go") == 1,
-		-- 	init = function()
-		-- 		vim.api.nvim_create_autocmd({ "LspAttach" }, {
-		-- 			pattern = { "*.go" },
-		-- 			callback = function(args)
-		-- 				require("cmp_go_pkgs").init_items(args)
-		-- 			end,
-		-- 		})
-		-- 	end,
-		-- },
+		{
+			"Yu-Leo/cmp-go-pkgs",
+			enabled = vim.fn.executable("go") == 1,
+			init = function()
+				vim.api.nvim_create_autocmd({ "LspAttach" }, {
+					pattern = { "*.go" },
+					callback = function(args)
+						require("cmp_go_pkgs").init_items(args)
+					end,
+				})
+			end,
+		},
 		-- Snippet Engine
 		{
 			"L3MON4D3/LuaSnip",
