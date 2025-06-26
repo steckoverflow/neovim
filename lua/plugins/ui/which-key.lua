15fd585e354fbd58d8277da91be80c4d935d0eb9
Simple-MAX <martin@techmdw.com>
2025-06-26 14:15:26 +0200

    refactor: move plugin for indent-blankline, noice, rainbow-delimiters, lualine, and which-key plugins to ui

---
+++ lua/plugins/ui/which-key.lua
@@ -0,0 +1,34 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	dependencies = { "echasnovski/mini.icons", "nvim-tree/nvim-web-devicons" },
+	opts = {
+		preset = "modern",
+		delay = 0,
+		spec = {
+			{ "<leader>f",  group = "Format" },
+			{ "<leader>g",  group = "Git" },
+			{ "<leader>h",  group = "Git Hunk",        mode = { "n", "v" } },
+			{ "<leader>r",  group = "Refactor",        mode = { "n", "v" } },
+			{ "<leader>s",  group = "Search",          mode = { "n", "v" } },
+			{ "<leader>sn", group = "Search Neovim" },
+			{ "<leader>t",  group = "Terminal" },
+			{ "gp",         group = "Preview" },
+			{ "<leader>ap", group = "avante: prompts", mode = { "n", "v" } },
+			{ "<leader>u",  group = "Ui" },
+			{ "<leader>c",  group = "Code",            mode = { "n", "v" } },
+			{ "<leader>d",  group = "Debug" },
+			{
+				"<leader>S",
+				group = "Scratch",
+				icon = { icon = "󱓧", color = "red" },
+			},
+			{
+				"<leader>a",
+				group = "Avante",
+				mode = { "n", "v" },
+				icon = { icon = "", color = "yellow" },
+			},
+		},
+	},
+}
